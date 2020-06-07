ac02a12a0855e42959c515c83cd58a7a1eec8281
Dũng <qngnud@gmail.com>
2020-06-07 14:49:04 +0700

    fix bug can't build project

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HelloWorld from './src/containers/HelloWorldContainer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-
+import 'react-native-gesture-handler';
 const Stack = createStackNavigator();
 
 const App: React.FC = () => {
