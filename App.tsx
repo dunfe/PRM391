85f7540d606cc91b97c5df9ccde21a76325f0926
Dũng <qngnud@gmail.com>
2020-06-07 16:14:45 +0700

    commit before run react-codemods

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import HelloWorld from './src/containers/HelloWorldContainer';
+import Auth from './src/screen/Auth';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
+import Login from './src/screen/Login';
 const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Hello World" component={HelloWorld}/>
+        <Stack.Screen name="Home" component={Auth}/>
+        <Stack.Screen name="Login" component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
