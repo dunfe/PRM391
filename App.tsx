129cb421b695f6aaa5e60e86312eee0bf69f425c
namnd <namnd@vnext.com.vn>
2020-07-01 15:27:00 +0700

    Screen Home

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Auth from './src/screen/Auth';
+// import Auth from './src/screen/Auth';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
-import Login from './src/screen/Login';
-import Register from './src/screen/Register';
-import Secured from './src/screen/Secured';
-
+// import Login from './src/screen/Login';
+// import Register from './src/screen/Register';
+// import Secured from './src/screen/Secured';
+import productList from './src/screen/ProductList';
+import Home from './src/screen/ScreenHome';
 // @TODO: This is to hide a Warning
 //  caused by NativeBase after upgrading to RN 0.62
 import {YellowBox} from 'react-native';
+import ProductListScreen from './src/screen/ProductList';
 
 YellowBox.ignoreWarnings([
   'Animated: `useNativeDriver` was not specified. ' +
@@ -17,18 +19,20 @@ YellowBox.ignoreWarnings([
 ]);
 // ------- END OF WARNING SUPPRESSION
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={Auth}/>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Secured" component={Secured}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home/>
+    // <NavigationContainer>
+    //   <Stack.Navigator screenOptions={{headerShown: false}}>
+    //     <Stack.Screen name ="home" component={ProductListScreen}/>
+    //     <Stack.Screen name="Home" component={Auth}/>
+    //     <Stack.Screen name="Login" component={Login}/>
+    //     <Stack.Screen name="Register" component={Register}/>
+    //     <Stack.Screen name="Secured" component={Secured}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 };
 
---
+++ src/screen/ScreenHome.tsx
@@ -0,0 +1,141 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator}
+  from '@react-navigation/material-bottom-tabs';
+import {View, Text} from 'react-native';
+import ProductList from './ProductList';
+// import {Icon} from 'native-base';
+// import MaterialCommunityIcons from 'react-native-vector-icons'
+import Icon from 'react-native-vector-icons/Feather';
+
+const ProductListDis = () => {
+  return (
+    <View>
+      <ProductList />
+    </View>
+  );
+};
+
+const Feed = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Feed!</Text>
+    </View>
+  );
+};
+
+const Remark = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Remark!</Text>
+    </View>
+  );
+};
+
+const Cart = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Cart!</Text>
+    </View>
+  );
+};
+
+const Notification = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Notification!</Text>
+    </View>
+  );
+};
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName='ProductListDis'
+      activeColor='#FFC529'
+      inactiveColor='#D7D7D7'
+      // style={{backgroundColor: '#ffffff'}}>
+      barStyle={{backgroundColor: '#ffffff'}}>
+      <Tab.Screen
+        name="ProductListDis"
+        component={ProductListDis}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarColor: '#ffffff',
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: ({color}) => (
+            // <Feather name='home'/>
+            <Icon name='home'
+              style={{fontSize: 20, color: color}}/>
+            // <MaterialCommunityIcons name="home" size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Menu',
+          tabBarColor: '#ffffff',
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: ({color}) => (
+            <Icon name='book-open'
+              style={{fontSize: 20, color: color}}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarColor: '#ffffff',
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: ({color}) => (
+            <Icon name='shopping-bag'
+              style={{fontSize: 20, color: color}}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Remark"
+        component={Remark}
+        options={{
+          tabBarLabel: 'Remark',
+          tabBarColor: '#ffffff',
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: ({color}) => (
+            <Icon name='bookmark'
+              style={{fontSize: 20, color: color}}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarLabel: 'Notification',
+          tabBarColor: '#ffffff',
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: ({color}) => (
+            <Icon name='bell'
+              style={{fontSize: 20, color: color}}/>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+};
+
+export default App;
