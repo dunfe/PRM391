ca2e743c550c82c06a888e0a437c33ec1a69632e
namnguyen1209 <64149682+namnguyen1209@users.noreply.github.com>
2020-07-06 23:00:29 +0700

    Revert "Merge pull request #8 from qngnud/namnd"
    This reverts commit a738b3af6e330048ffe602fee6bd77aaa87f416c.

--- App.tsx
+++ App.tsx
@@ -3,18 +3,11 @@ import Auth from './src/screen/Auth';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
+import Secured from './src/screen/Secured';
 
-import Login from './src/screen/Login';
-import Register from './src/screen/Register';
-import Secured from './src/screen/Secured';
-//import productList from './src/screen/ProductList';
-//import Home from './src/screen/ScreenHome';
-=======
-import Secured from './src/screen/Secured';
 // @TODO: This is to hide a Warning
 //  caused by NativeBase after upgrading to RN 0.62
 import {YellowBox} from 'react-native';
-//import ProductListScreen from './src/screen/ProductList';
 
 YellowBox.ignoreWarnings([
   'Animated: `useNativeDriver` was not specified. ' +
@@ -27,20 +20,11 @@ const Stack = createStackNavigator();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name ="home" component={ProductListScreen}/>
-        <Stack.Screen name="Home" component={Auth}/>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Secured" component={Secured}/>
-        </Stack.Navigator>
-    </NavigationContainer>
-<!--     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={Auth}/>
         <Stack.Screen name="Secured" component={Secured}/>
       </Stack.Navigator>
-    </NavigationContainer> -->
+    </NavigationContainer>
   );
 };
 
