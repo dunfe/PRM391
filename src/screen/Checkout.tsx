a02777144b71c1e80cb6a127064a194b79693dfb
Dung <qngnud@gmail.com>
2020-07-23 17:15:24 +0700

    add momo

--- src/screen/Checkout.tsx
+++ src/screen/Checkout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -15,6 +15,11 @@ import IconAwesome from 'react-native-vector-icons/FontAwesome5';
 import {useSelector} from "react-redux";
 // eslint-disable-next-line no-unused-vars
 import {CartState} from "../containers/cartInterface";
+import {Platform, NativeModules, NativeEventEmitter} from 'react-native';
+// @ts-ignore
+import RNMomosdk from 'react-native-momosdk';
+const RNMoMoPaymentModule = NativeModules.RNMomosdk;
+const EventEmitter = new NativeEventEmitter(RNMoMoPaymentModule);
 
 const cards = [
   {
@@ -40,14 +45,184 @@ const cards = [
   },
 ];
 
+const merchantname: string = "Food Order";
+const merchantcode: string = "MOMOXTMJ20200723";
+const merchantNameLabel: string = "Nhà cung cấp";
+const billdescription: string = "Total Cart";
+const amountString: number = 50000;
+const enviroment: string = "0"; // "1": production
+
+const formatNumberToMoney = (number: any,
+    defaultNum: any,
+    predicate: any) => {
+  predicate = !predicate ? "" : "" + predicate;
+  if (number == 0 ||
+      number == '' ||
+      number == null ||
+      number == 'undefined' ||
+      isNaN(number) ||
+      number == '0' ||
+      number == '00' ||
+      number == '000') {
+    return "0" + predicate;
+  }
+
+  const array = [];
+  let result = '';
+  let count = 0;
+
+  if (!number) {
+    return defaultNum ? defaultNum : "" + predicate;
+  }
+
+  let flag1 = false;
+  if (number < 0) {
+    number = -number;
+    flag1 = true;
+  }
+
+  const numberString = number.toString();
+  if (numberString.length < 3) {
+    return numberString + predicate;
+  }
+
+  for (let i = numberString.length - 1; i >= 0; i--) {
+    count += 1;
+    if (numberString[i] == "." || numberString[i] == ",") {
+      array.push(',');
+      count = 0;
+    } else {
+      array.push(numberString[i]);
+    }
+    if (count == 3 && i >= 1) {
+      array.push('.');
+      count = 0;
+    }
+  }
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    result += array[i];
+  }
+
+  if (flag1) {
+    result = "-" + result;
+  }
+
+  return result + predicate;
+};
+
+
 const Checkout = () => {
   const numOfProduct = 0;
   const foodPrice = 100;
   const deliveryPrice = 0;
+  const [amount, setAmount] = useState(amountString);
+  const [textAmount, setTextAmount] = useState(
+      formatNumberToMoney(amount, null, ""));
+  const [description, setDescription] = useState('');
+  const [processing, setProcessing] = useState(false);
   const clickHandler = () => {
     Alert.alert('Floating Button Clicked');
   };
   const cart = useSelector((state: CartState) => state.cart);
+
+
+  useEffect(() => {
+    const listenAction = () => {
+      setAmount(cart.total);
+      setTextAmount(formatNumberToMoney(cart.total, null, ""));
+      EventEmitter.addListener(
+          'RCTMoMoNoficationCenterRequestTokenReceived', (response) => {
+            console.log("<MoMoPay>Listen.Event::" + JSON.stringify(response));
+            try {
+              if (response && response.status == 0) {
+                const fromapp = response.fromapp;
+                setDescription(JSON.stringify(response));
+                setProcessing(false);
+                const momoToken = response.data;
+                const phonenumber = response.phonenumber;
+                const message = response.message;
+                const orderId = response.refOrderId; // your orderId
+                const requestId = response.refRequestId; // your requestId
+                // continue to submit momoToken,phonenumber to server
+                console.log(fromapp +
+                    momoToken +
+                    phonenumber +
+                    message +
+                    orderId +
+                    requestId);
+              } else {
+                setDescription("message: Get token fail");
+                setProcessing(false);
+              }
+            } catch (ex) {
+              console.log(ex);
+            }
+          });
+
+      // OPTIONAL
+      EventEmitter.addListener(
+          'RCTMoMoNoficationCenterRequestTokenState', (response) => {
+            console.log("<MoMoPay>Listen.RequestTokenState:: " +
+                response.status);
+            // status = 1: Parameters valid & ready to open MoMo app.
+            // status = 2: canOpenURL failed for URL MoMo app
+            // status = 3: Parameters invalid
+          });
+    };
+    listenAction();
+  }, []);
+
+  const momoHandleResponse = async (response: any) =>{
+    try {
+      if (response && response.status == 0) {
+        const fromapp = response.fromapp;
+        setDescription(JSON.stringify(response));
+        setProcessing(false);
+        const momoToken = response.data;
+        const phonenumber = response.phonenumber;
+        const message = response.message;
+        // continue to submit momoToken,phonenumber to server
+        console.log(fromapp + momoToken + phonenumber + message);
+      } else {
+        setDescription("message: Get token fail");
+        setProcessing(false);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const onPress = async () => {
+    if (!processing) {
+      const jsonData: any = {};
+      jsonData.enviroment = enviroment;
+      jsonData.action = "gettoken";
+      jsonData.isDev = true; // SANBOX only , remove this key on PRODUCTION
+      jsonData.merchantname = merchantname;
+      jsonData.merchantcode = merchantcode;
+      jsonData.merchantnamelabel = merchantNameLabel;
+      jsonData.description = billdescription;
+      jsonData.amount = amount;
+      jsonData.orderId = "bill234284290348";
+      jsonData.requestId = "bill234284290348";
+      jsonData.orderLabel = "Ma don hang";
+      jsonData.appScheme = "momoxtmj20200723";
+      console.log("data_request_payment " + JSON.stringify(jsonData));
+      if (Platform.OS === 'android') {
+        const dataPayment = await RNMomosdk.requestPayment(jsonData);
+        await momoHandleResponse(dataPayment);
+        console.log("data_request_payment " + dataPayment.status);
+      } else {
+        RNMomosdk.requestPayment(JSON.stringify(jsonData));
+      }
+      setDescription("");
+      setProcessing(false);
+    } else {
+      setDescription("...");
+      setProcessing(false);
+    }
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
@@ -118,13 +293,13 @@ const Checkout = () => {
           <View>
             <Text style={styles.totalPriceText}>
               <IconAwesome name="dollar-sign" color="#FFC529" size={20} />
-              <Text> </Text>
-              {cart.total}
+              <Text>{description}</Text>
+              {textAmount}
             </Text>
           </View>
         </View>
       </View>
-      <TouchableOpacity style={styles.ConfirmButton}>
+      <TouchableOpacity onPress={onPress} style={styles.ConfirmButton}>
         <Text style={styles.ConfirmText}>Confirm Order</Text>
       </TouchableOpacity>
     </ScrollView>
